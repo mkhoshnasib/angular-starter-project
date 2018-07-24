35933552eb3a6ceed95a85033262e0653d963b17
Majid Khoshnasib <majid.khoshnasib@gmail.com>
2018-07-24 12:58:08 +0430

    unnecessary toastr removed.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,12 +6,4 @@ import {ToastrService} from 'ngx-toastr';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor(private toastr: ToastrService) {
-    this.showSuccess();
-  }
-
-  showSuccess() {
-    this.toastr.success('Hello world!', 'Toastr fun!');
-  }
 }
