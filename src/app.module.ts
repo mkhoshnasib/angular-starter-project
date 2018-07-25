da5157a7f92aa8c0054c84d588c3f7ece69aea86
Majid Khoshnasib <majid.khoshnasib@gmail.com>
2018-07-25 11:32:58 +0430

    jquery version changed.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,29 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import {AppComponent} from './app/app.component';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {ToastrModule, ToastrService} from 'ngx-toastr';
-import {HttpHandler} from '@angular/common/http';
-import {Http} from './app/services/http/http-interceptor.service';
-import {LoginComponent} from './app/pages/login/login.component';
-import {ReactiveFormsModule} from '@angular/forms';
-import {routes} from './app/routers/app.router';
-import { HomeComponent } from './app/pages/home/home.component';
-import {GuardService} from './app/services/guard/guard.service';
+import {AppComponent} from "./app/app.component";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {ToastrModule, ToastrService} from "ngx-toastr";
+import {HttpHandler} from "@angular/common/http";
+import {Http} from "./app/services/http/http-interceptor.service";
+import {LoginComponent} from "./app/pages/login/login.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {routes} from "./app/routers/app.router";
+import {HomeComponent} from "./app/pages/home/home.component";
+import {GuardService} from "./app/services/guard/guard.service";
+import {StoreComponent} from "./app/pages/store/store.component";
+import {CartComponent} from "./app/pages/cart/cart.component";
+import {ProductCardComponent} from "./app/components/product-card/product-card.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    StoreComponent,
+    CartComponent,
+    ProductCardComponent
   ],
   imports: [
     BrowserModule,
